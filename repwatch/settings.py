f2e03c5fe6f6f07c98a2d6591590522e88eda052
Gabriela Pacheco <gcpachecogonzalez@my.waketech.edu>
2026-02-11 20:17:39 -0500

    Adding files and modifing them

--- repwatch/settings.py
+++ repwatch/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
 
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/"
 
 
 # Password validation
@@ -143,7 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
