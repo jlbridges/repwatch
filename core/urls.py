e2130126609606cdc8ae9206402c7480aaa15422
Henry Vogel <145382058+HenVogel@users.noreply.github.com>
2026-02-19 23:26:31 -0500

    Add about path to URL patterns

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
-from .views import dashboard, registration, login_view, homepage
+from .views import dashboard, registration, login_view, homepage, about
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("accounts/login/", login_view, name="login"),
     path("accounts/signup/", registration, name="registration"),
     path("dashboard/", dashboard, name="dashboard"),
+    path("about/", about, name="about"),
     path ("logout/", auth_views.LogoutView.as_view(next_page=reverse_lazy('homepage')), name="logout" ) 
 ]
