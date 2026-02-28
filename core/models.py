9116201cf7a2efd318e440d68e00bbe15508cd89
rp-scruggs <rpscruggs@hotmail.com>
2026-02-28 15:49:43 -0500

    Adding representative model and making some profile values required

--- core/models.py
+++ core/models.py
@@ -9,11 +9,11 @@ class Profile(models.Model):
     }
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to auth user
-    address_line1 = models.CharField(max_length=35, blank=True, default="")
+    address_line1 = models.CharField(max_length=35, blank=False,null=False, default="")
     address_line2 = models.CharField(max_length=35, blank=True, default="")
-    city = models.CharField(max_length=50, blank=True, default="")
-    state = models.CharField(max_length=2, choices=STATE_LIST)
-    zipcode = models.CharField(max_length=10, blank=True, default="")
+    city = models.CharField(max_length=50, blank=False,null=False, default="")
+    state = models.CharField(max_length=2,blank=False,null=False, choices=STATE_LIST)
+    zipcode = models.CharField(max_length=10, blank=False,null=False,default="")
 
     class Meta:
         db_table = "repwatch_profile"
@@ -21,3 +21,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+
+
+class representative(models.Model):
+    Bioguide_id = models.CharField(max_length=10,blank=False, null=False, primary_key=True)
+    constituents = models.ManyToManyField(User)
+    name = models.CharField(max_length=70,blank=False)
+    district_number = models.IntegerField(null=False)
+    first_name = models.CharField(max_length=35,blank=False, null=False)
+    last_name = models.CharField(max_length=35, blank=False,null=False)
+    state = models.CharField(max_length=2, blank=False, null=False)
+    party = models.CharField(max_length=25,blank= False)
+    photo_url = models.URLField(max_length=40)
+    image = models.ImageField()
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=["last_name", "first_name"]),
+            models.Index(fields=["first_name"], name="first_name_idx"),
+            models.Index(fields=["Bioguide_id"],name="Bio_idx"),
+        ]
+def __str(self):
+    return f"{self.Bioguide_id}"
