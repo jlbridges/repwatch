b520ef57ca13402731dfcd3b1e2fb56e6ef9e77b
Joseph Baynard <jsbaynard@my.waketech.edu>
2026-04-29 20:41:21 -0400

    Updated test cases to follow current procedures, fixed address validation test bug

--- tests/Dashboard Testing/test_save_bill.py
+++ tests/Dashboard Testing/test_save_bill.py
@@ -1,11 +1,13 @@
 import pytest
+from unittest.mock import patch
 from django.urls import reverse
 from core.models import BillHeader
 
 pytestmark = pytest.mark.django_db
 
 
-def test_user_can_save_bill(client, test_user, test_password, login_url):
+@patch("core.services.bill_service.save_bill_detail")
+def test_user_can_save_bill(mock_detail, client, test_user, test_password, login_url):
     client.post(login_url, {
         "email": test_user.email,
         "password": test_password,
@@ -18,9 +20,13 @@ def test_user_can_save_bill(client, test_user, test_password, login_url):
         title="Test Bill"
     )
 
-    response = client.post(reverse("save_bill", args=[bill.id]))
+    response = client.post(reverse("save_bill", args=[bill.number]), {
+        "congress": bill.congress,
+        "type": bill.type,
+        "title": bill.title,
+    })
 
     bill.refresh_from_db()
 
-    assert test_user in bill.tracked_by.all()
+    assert test_user in bill.saved_by.all()
     assert response.status_code == 302
